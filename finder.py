3dd3b45247688b7b364a2fb1562962f14458f134
Caleb Ely <le717@users.noreply.github.com>
2020-05-05 12:17:28 -0400

    Add entrypoint script

---
+++ finder.py
@@ -0,0 +1,87 @@
+import argparse
+import logging
+import sys
+from typing import Dict
+
+import sys_vars
+
+# from apscheduler.schedulers.blocking import BlockingScheduler
+# from apscheduler.schedulers.background import BackgroundScheduler
+
+# Handle app arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-f",
+    "--fetch",
+    help="attempt to automatically record the latest prompt. Cannot be combined with other arguments",
+    action="store_true",
+)
+parser.add_argument(
+    "-m",
+    "--manual",
+    help="manually record a specific prompt. Cannot be combined with other arguments",
+    action="store_true",
+)
+parser.add_argument(
+    "-s",
+    "--schedule",
+    help="schedule recording the latest prompt according to ENV values. Cannot be combined with other arguments",
+    action="store_true",
+)
+args = parser.parse_args()
+
+# Create a logger to print all logging output to stdout
+logger = logging.getLogger("vss365today-finder")
+logger.setLevel(logging.INFO)
+LOG_FORMAT = logging.Formatter("[%(asctime)s - %(levelname)s]: %(message)s")
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(LOG_FORMAT)
+logger.addHandler(handler)
+
+
+if __name__ == "__main__":
+    # Find out which arguments were passed
+    argument_values: Dict[str, bool] = vars(args)
+    number_of_args_passed = len([v for v in argument_values.values() if v])
+
+    # All arguments were passed and that is not allowed
+    if number_of_args_passed >= 2:
+        print(
+            f"{__file__}: error: cannot combine arguments. Only a single argument is permitted"
+        )
+        raise SystemExit(1)
+
+    # Manually enter a prompt
+    if args.manual:
+        print("manual")
+        raise SystemExit(0)
+
+    # Attempt to automatically find the latest prompt right now
+    elif args.fetch:
+        print("fetch")
+        raise SystemExit(0)
+
+    # Schedule finding the latest prompt
+    elif args.schedule:
+        print("schedule")
+        # scheduler = BackgroundScheduler()
+        # Get the scheduled times
+        schedule_times = sys_vars.get_json("SCHEDULE_TIMES")
+        for time in schedule_times:
+            hour, minute = time.split()
+        #     scheduler.add_job(
+        #         (lambda: print("job")),
+        #         args=[],
+        #         trigger="cron",
+        #         hour=hour,
+        #         minute=minute,
+        #         day_of_week="*",
+        #     )
+        # scheduler.get_jobs()
+        # scheduler.start()
+        # raise SystemExit(0)
+
+    # No arguments were passed
+    else:
+        parser.print_help()
+        raise SystemExit(0)
