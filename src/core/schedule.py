9aca85531a10f39ddde98f000606c724dafa5fc0
Caleb Ely <le717@users.noreply.github.com>
2020-07-12 20:25:23 -0400

    Schedule the archive task

--- src/core/schedule.py
+++ src/core/schedule.py
@@ -3,7 +3,7 @@ from typing import List
 from apscheduler.schedulers.blocking import BlockingScheduler
 import sys_vars
 
-from src.core import fetch
+from src.core import archive, fetch
 
 __all__ = ["main"]
 
@@ -25,6 +25,20 @@ def main():
             minute=minute,
             day_of_week="*",
         )
+
+    # Set the run time to be 5 minutes after the scheduled one
+    minute, hour = schedule_times[-1].split()
+    minute = str(int(minute) + 5)
+
+    # Schedule the word archive generation
+    scheduler.add_job(
+        archive.main,
+        args=[],
+        trigger="cron",
+        hour=hour,
+        minute=minute,
+        day_of_week="*",
+    )
 
     # Start the scheduler
     scheduler.start()
