e9d8823826c3dc93039566b0b56bc6cb55c5dc48
Caleb Ely <le717@users.noreply.github.com>
2020-12-19 23:14:32 -0500

    Remove unused method

--- src/helpers/date.py
+++ src/helpers/date.py
@@ -1,14 +1,9 @@
 from datetime import datetime
 
 
-__all__ = ["create_datetime", "format_datetime"]
+__all__ = ["create_datetime"]
 
 
 def create_datetime(date_str: str) -> datetime:
     """Create a datetime object from an ISO 8601 date string."""
     return datetime.fromisoformat(date_str.strip())
-
-
-def format_datetime(datetime_obj: datetime) -> str:
-    """Format a datetime object as YYYY-MM-DD."""
-    return datetime_obj.strftime("%Y-%m-%d")
