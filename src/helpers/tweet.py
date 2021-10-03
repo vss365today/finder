9ba286f4727d667c3408aacd28697d47d382821b
Caleb Ely <le717@users.noreply.github.com>
2021-10-03 16:09:22 -0400

    Fix prompt_index indexing

--- src/helpers/tweet.py
+++ src/helpers/tweet.py
@@ -98,7 +98,7 @@ def get_prompt(tweet: namedtuple) -> Optional[str]:
     hts = tweet.data.entities["hashtags"]
     hts = __filter_hashtags(__get_hashtags(hts))
     print(hts)
-    return hts[CONFIG["prompt_index"]]
+    return hts[CONFIG["prompt_index"] + 1]
 
 
 def get_text(tweet: namedtuple) -> str:
