4442839d80e2fdeee50cfdadfc589f40faf55905
Caleb Ely <le717@users.noreply.github.com>
2022-11-27 16:09:38 -0500

    Add a deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Get the latest code
+git pull
+
+# Build a new container
+docker build -t 0 vss365today-finder:latest
+
+# Restart the service
+cd ../server
+docker stop -t 0 finder
+docker-compose up -d finder
+cd ../finder
